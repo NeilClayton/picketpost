140ea79eb36d69380adbdf5135036a30fa4a1274
Neil Clayton <ncairrifle@outlook.com>
2018-11-19 20:53:25 +0000

    add initial controllers models migrations and single view

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class TicketsController extends Controller
+{
+    
+	public function create()
+	{
+		$categories = Category::all();
+
+		return view('tickets.create', compact('categories'));
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('new_ticket', 'TicketsController@create');
+Route::post('new_ticket', 'TicketsController@store');
+
+Route::get('/home', 'HomeController@index')->name('home');
