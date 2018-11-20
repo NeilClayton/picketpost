153dbc2edc20de8c39bdbf59f5871530218ac80e
Neil Clayton <ncairrifle@outlook.com>
2018-11-20 19:49:24 +0000

    add store method to TicketController

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Ticket;
 use Illuminate\Http\Request;
 use App\Category;
 
@@ -13,6 +14,31 @@ class TicketsController extends Controller
 		$categories = Category::all();
 
 		return view('tickets.create', compact('categories'));
+	}
+
+	public function store(Request $request/*, AppMailer $mailer*/)
+	{
+		$this->validate($request, [
+			'title'		=> 'required',
+			'category'	=> 'required',
+			'priority'	=> 'required',
+			'message'	=> 'required',
+		]);
+		
+		$ticket = new Ticket([
+			'title'			=> $request->input('title'),
+			'user_id'		=> \Auth::user()->id,
+			'ticket_id'		=> strtoupper(str_random(10)),
+			'category_id'	=> $request->input('category'),
+			'priority'		=> $request->input('priority'),
+			'message'		=> $request->input('message'),
+			'status'		=> "Open"
+		]);
+
+		$ticket->save();
+		//TODO: Uncomment when app mailer is created
+		//$mailer->sendTicketInformation(Auth::user(), $ticket);
+		return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened.");
 	}
 
 }
